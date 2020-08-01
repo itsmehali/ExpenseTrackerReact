ddf2adf4098b3d4569716581dea15602471cd59f
itsmehali <halaszbalazs9825@gmail.com>
2020-08-01 14:10:41 +0200

    Updates

---
+++ precache-manifest.0fe6b4f2f3df4828c01300b69187cf0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b11701391a46000097562f70258ee4b",
+    "url": "/ExpenseTrackerReact/index.html"
+  },
+  {
+    "revision": "552a67605481717c7eca",
+    "url": "/ExpenseTrackerReact/static/css/main.f62e9807.chunk.css"
+  },
+  {
+    "revision": "1999f2e13e76ad44b252",
+    "url": "/ExpenseTrackerReact/static/js/2.a91c904f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ExpenseTrackerReact/static/js/2.a91c904f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "552a67605481717c7eca",
+    "url": "/ExpenseTrackerReact/static/js/main.e1147c0b.chunk.js"
+  },
+  {
+    "revision": "ca82093f376eeca24bf7",
+    "url": "/ExpenseTrackerReact/static/js/runtime-main.21c04c9b.js"
+  }
+]);
